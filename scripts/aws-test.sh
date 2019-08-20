5cd427320b676c0d287461a0bfa42a81ad80e0ec
Jordancooke <9076311+Jordancooke@users.noreply.github.com>
2019-08-20 09:26:36 +0100

    Update

--- scripts/aws-test.sh
+++ scripts/aws-test.sh
@@ -3,7 +3,5 @@
 # Should only trigger when requirements have changed.
 pip install -r aws-deploy/dev-requirements.txt
 
-pwd
-ls aws-deploy
-#flake8 /aws-deploy/python/
-#pytest /aws-deploy/python/
+flake8 aws-deploy/python/
+pytest aws-deploy/python/
