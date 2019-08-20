07b9e8d17f9bff55f650585354652256b43a4fc9
Jordancooke <9076311+Jordancooke@users.noreply.github.com>
2019-08-20 16:18:29 +0100

    Fixing

--- scripts/aws-test.sh
+++ scripts/aws-test.sh
@@ -3,5 +3,6 @@
 # Should only trigger when requirements have changed.
 pip install -r GIT-repository/dev-requirements.txt
 
+# Run Linter
 flake8 GIT-repository/python/
 pytest GIT-repository/python/
