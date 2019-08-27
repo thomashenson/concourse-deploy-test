5688d21206ac89e88ea61b808352585c5db0f2cc
Jordancooke <9076311+Jordancooke@users.noreply.github.com>
2019-08-27 16:07:32 +0100

    Updating linter to fail on find

--- docker/aws-test.sh
+++ docker/aws-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 # Run flake8 linting
-flake8 GIT-repository/python/
+flake8 GIT-repository/python/ && echo "flake8 passed. Yayyy." || exit 1
 
 # Run unit tests
 pytest GIT-repository/python/
