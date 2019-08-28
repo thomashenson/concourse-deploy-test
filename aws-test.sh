d61b6d39f4fd9ba935562c7ecf4e8bd939582312
thomashenson <thomasjameshenson@gmail.com>
2019-08-28 12:37:15 +0100

    Fixed problem with serverless yml. Moved script files to root directory.

--- aws-test.sh
+++ aws-test.sh
@@ -3,9 +3,9 @@
 # set -e
 
 # Run flake8 linting
-flake8 GIT-repository/python/ 
+flake8 GIT-repository 
 
 # && echo "flake8 passed. Yayyy." || exit 1
 
 # Run unit tests
-pytest GIT-repository/python/
+pytest GIT-repository
