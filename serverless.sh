25c1a7ff67b00af197024a20e6293fbe9cc6e876
“thomashenson” <thomasjameshenson@gmail.com>
2019-09-13 13:25:48 +0100

    Trying to fix serverless.

--- serverless.sh
+++ serverless.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#echo sls version: `serverless --version`
-#echo current dir: `pwd`
-#echo dir listing: 
-#ls
+echo sls version: `serverless --version`
+echo current dir: `pwd`
+echo dir listing: 
+ls
 cd GIT-repository
-#ls
-#echo packaging serverless bundle...
-#serverless package --package pkg
-#find ./pkg
+ls
+echo packaging serverless bundle...
+serverless package --package pkg
+find ./pkg
 serverless deploy --verbose; 
